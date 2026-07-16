4259d04c52cda5090fa5b172e63dadaf68242ac2
yunzelu <ylu239@uottawa.ca>
2026-07-16 01:03:02 -0400

    jobs

--- thesis/e2/infer.sh
+++ thesis/e2/infer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-mbolic
-#SBATCH --gpus=a100_4g.20gb:1
+#SBATCH --gpus=a100_3g.20gb:1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=31G
-#SBATCH --time=04:00:00
+#SBATCH --time=02:00:00
 #SBATCH --job-name=e2_infer
 #SBATCH --output=thesis/e2/%x_%j.out
 #SBATCH --mail-user=yunzelu@outlook.com
@@ -16,5 +16,5 @@ cd ~/projects/def-mbolic/yunzelu/pyskl/
 
 export CUDA_VISIBLE_DEVICES=0
 
-python thesis/e2/infer_scores.py --stream joint --overwrite
+# python thesis/e2/infer_scores.py --stream joint --overwrite
 python thesis/e2/infer_scores.py --stream limb  --overwrite
